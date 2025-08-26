66ee3aa0c0de0755f6970d35754800f0a0fc8046
johnbyeon <ghks0115@gmail.com>
2025-08-26 12:03:36 +0900

    양방향 연관관계 설정

---
+++ src/main/java/com/jpaTest/jpaTest/entitiy/Team.java
@@ -0,0 +1,31 @@
+package com.jpaTest.jpaTest.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Team {
+    @Id
+    private  String teamId;
+    private  String teamName;
+
+
+    //팀에 소속된 멤버의 리스트 저장
+    //연관관계 설정 :Member 클래스의 Team 정보를 가리킴
+    @OneToMany(mappedBy = "team",fetch = FetchType.EAGER)
+    private List<Member> memberList = new ArrayList<>();
+}
---
+++ src/test/java/com/jpaTest/jpaTest/service/RelationTestServiceTest.java
@@ -0,0 +1,65 @@
+package com.jpaTest.jpaTest.service;
+
+import com.jpaTest.jpaTest.entitiy.Member;
+import com.jpaTest.jpaTest.entitiy.Team;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class RelationTestServiceTest {
+    @Autowired
+    RelationTestService releationTestService;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("MemberAndTeam Insert Test")
+    void InsertTest() {
+        releationTestService.insertMemberAndTeam();
+    }
+
+    @Test
+    @DisplayName("단방향 연관관계 설정후 팀이름 찾기")
+    void findTeamName() {
+        //저장 서비스 호출
+
+        Member jang = em.find(Member.class, "jang");
+
+        System.out.println("팀이름 : " + jang.getTeam().getTeamName());
+
+    }
+
+    @Test
+    @DisplayName("양방향 연관관계 설정 후 저장하기")
+    void saveBothData() {
+        releationTestService.insertBothRelation();
+
+    }
+
+    //@Test
+    //@DisplayName("ID:jang의 팀이름 찾기 테스트")
+    //void findTeamName(){
+    //    Member jang = em.find(Member.class,"jang");
+    //    Team team = em.find(Team.class,jang.getTeamId());
+    //    System.out.println("Team Name : " + team.getTeamName());
+    //}
+    @Test
+    @DisplayName("씨름팀 멤버 출력")
+    void 씨름팀_멤버출력(){
+        Team team = em.find(Team.class,"씨름");
+        List<Member> members = team.getMemberList();
+        for(Member m : members){
+            System.out.println(m.getName());
+        }
+    }
+}
